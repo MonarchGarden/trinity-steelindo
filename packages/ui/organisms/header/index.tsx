ad6a3eba3d27c54b9e8f52e2594c1484bcc6daa1
TheChefBigDog <bobbyryan692@gmail.com>
2023-11-23 00:43:33 +0700

    Check again
    + Igonre ts warning

--- packages/ui/organisms/header/index.tsx
+++ packages/ui/organisms/header/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavbarBackground} from '@trinity-steelindo/ui/molecules';
 import '../../index.css';
 
+// @ts-ignore: TS6133
 export const Header = () => {
   return <NavbarBackground>Test</NavbarBackground>;
 };
