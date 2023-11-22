61950e95d6a4513162b6341a115d52e509cb0c22
TheChefBigDog <bobbyryan692@gmail.com>
2023-11-23 01:02:02 +0700

    Check again
    + Check and revert

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './index.css';
+import {SvgIconClose, SvgIconMenu} from './assets';
+import {Header} from '@trinity-steelindo/ui/organisms';
 
 export default function App() {
   return (
-    // <Header iconMenuClose={<SvgIconClose />} iconMenuOpen={<SvgIconMenu />}>
-    <div>Test</div>
-    // </Header>
+    <Header iconMenuClose={<SvgIconClose />} iconMenuOpen={<SvgIconMenu />} />
   );
 }
