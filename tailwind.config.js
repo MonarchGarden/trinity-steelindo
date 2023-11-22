0d181957664b8a8b559a8393f926dd9fd57e7316
TheChefBigDog <bobbyryan692@gmail.com>
2023-11-23 00:27:42 +0700

    Chnage Log:
    + Solved tailwind configuration export

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{html,js,ts,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{html,js,ts,tsx}',
+    './packages/**/*.{html,js,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
