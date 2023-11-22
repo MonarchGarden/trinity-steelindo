21936934276dadda927fbad14c431c8315e37da0
TheChefBigDog <bobbyryan692@gmail.com>
2023-11-23 00:32:01 +0700

    Check

--- packages/ui/atoms/company-logo/index.tsx
+++ packages/ui/atoms/company-logo/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import './style.css';
-type Props = {
-  imgSrc?: string;
-};
 
-export const CompanyLogo = ({imgSrc}: Props) => {
+export const CompanyLogo = () => {
   return (
     <div>
       <h1 className="text-black">Logo</h1>
