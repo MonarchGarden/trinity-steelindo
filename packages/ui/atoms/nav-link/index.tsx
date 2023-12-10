e3bb7524459220eff6bda39570bb471b536ba4f3
TheChefBigDog <bobbyryan692@gmail.com>
2023-12-10 17:59:28 +0700

    Continue Later On

--- packages/ui/atoms/nav-link/index.tsx
+++ packages/ui/atoms/nav-link/index.tsx
@@ -2,14 +2,20 @@ import React, {PropsWithChildren} from 'react';
 
 type Props = PropsWithChildren & {
   navBarTitle?: string;
+  isHeaderHovered: boolean;
 };
 
-export const NavButtonLink = ({navBarTitle}: Props) => {
+export const NavButtonLink = ({navBarTitle, isHeaderHovered}: Props) => {
   return (
     <a
       href="/"
-      className="group-hover relative border-b-2 border-transparent px-3 py-2 text-sm font-bold text-white hover:text-black">
-      <span className="border-colorPrimary absolute bottom-0 left-0 h-0 w-0 border-b-2 transition-all group-hover:w-full"></span>
+      className={`relative border-b-2 border-transparent px-3 py-2 text-sm font-bold ${
+        isHeaderHovered ? 'text-colorPrimary' : 'text-white'
+      } hover:text-colorPrimary`}>
+      <span
+        className={`absolute bottom-0 left-0 h-0 w-0 border-b-2 border-colorPrimary transition-all ${
+          isHeaderHovered ? '' : 'group-hover:w-full'
+        }`}></span>
       {navBarTitle}
     </a>
   );
--- packages/ui/organisms/header/index.tsx
+++ packages/ui/organisms/header/index.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
 import {NavButtonLink} from '@trinity-steelindo/ui/atoms';
 
 //TODO as of now everything is hardcoded
 export const Header = () => {
+  //Forced to use useState to change the navbuttonLink Text
+  const [isHeaderHovered, setHeaderHovered] = useState(false);
+
   return (
-    <header className="group fixed top-0 flex w-full overflow-hidden p-3 transition-all hover:bg-white">
+    <header
+      className="group fixed top-0 hidden w-full overflow-hidden p-3 transition-all hover:bg-white group-hover:text-black md:flex"
+      onMouseEnter={() => setHeaderHovered(true)}
+      onMouseLeave={() => setHeaderHovered(false)}>
       <nav className="mx-auto ml-auto flex h-full max-w-screen-xl items-center justify-between px-4 sm:px-6 lg:px-8">
         <div className="flex items-center">
           <div className="header-logo hidden md:block">
             <div className="flex items-center">
-              <NavButtonLink navBarTitle="Beranda" />
-              <NavButtonLink navBarTitle="Katalog Produk" />
-              <NavButtonLink navBarTitle="Tentang Kami" />
+              <NavButtonLink
+                navBarTitle="Beranda"
+                isHeaderHovered={isHeaderHovered}
+              />
+              <NavButtonLink
+                navBarTitle="Katalog Produk"
+                isHeaderHovered={isHeaderHovered}
+              />
+              <NavButtonLink
+                navBarTitle="Tentang Kami"
+                isHeaderHovered={isHeaderHovered}
+              />
             </div>
           </div>
         </div>
