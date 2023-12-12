0770da71b6b113a252b9a25f5a0a0e714a09fb72
TheChefBigDog <bobbyryan692@gmail.com>
2023-12-13 01:47:27 +0700

    Checkpoint

--- packages/ui/atoms/nav-link/index.tsx
+++ packages/ui/atoms/nav-link/index.tsx
@@ -1,4 +1,4 @@
-import React, {PropsWithChildren} from 'react';
+import React, {PropsWithChildren, useState} from 'react';
 
 type Props = PropsWithChildren & {
   navBarTitle?: string;
@@ -6,14 +6,18 @@ type Props = PropsWithChildren & {
 };
 
 export const NavButtonLink = ({navBarTitle, isHeaderHovered}: Props) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <a
       href="/"
       className={`relative border-b-2 border-transparent px-3 py-2 text-sm font-bold ${
         isHeaderHovered ? 'text-colorPrimary' : 'text-white'
-      } hover:text-colorPrimary`}>
+      } hover:text-colorPrimaryh hover:border-colorPrimary`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}>
       <span
-        className={`absolute bottom-0 left-0 h-0 w-0 border-b-2 border-colorPrimary transition-all ${
+        className={`absolute bottom-0 left-0 h-0 w-0 transition-all ${
           isHeaderHovered ? '' : 'group-hover:w-full'
         }`}></span>
       {navBarTitle}
