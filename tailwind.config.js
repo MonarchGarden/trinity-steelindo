aac01f360ba28cb18491af7dd5f423ea950ee27d
TheChefBigDog <bobbyryan692@gmail.com>
2023-12-17 09:00:12 +0700

    Test again

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,13 +17,14 @@ module.exports = {
 
     //   '2xl': {min: '1536px'},
     // },
-    backgroundImage: (theme) => ({
+    backgroundImage: {
       IconTrinityTruckSecondFull:
-        "url('./src/assets/icon-trinity-truck-2-full-size.jpeg')",
+        "url('../src/assets/icon-trinity-truck-2-full-size.jpeg')",
       IconTrinityTruckSecondCropped:
-        "url('./src/assets/icon-trinity-truck-2.jpeg')",
-      TruckTrinityPng: "url('./src/assets/trinity-steelindo-truck-crop-1.jpg')",
-    }),
+        "url('../src/assets/icon-trinity-truck-2.jpeg')",
+      TruckTrinityPng:
+        "url('../src/assets/trinity-steelindo-truck-crop-1.jpg')",
+    },
     extend: {
       backgroundColor: {
         colorPrimary: '#081923',
