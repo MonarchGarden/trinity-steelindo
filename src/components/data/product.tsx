4442ae39c23dc551a2584c99c30c33382ef031f3
TheChefBigDog <bobbyryan692@gmail.com>
2023-12-28 11:42:32 +0700

    Test

--- src/components/data/product.tsx
+++ src/components/data/product.tsx
@@ -17,13 +17,23 @@ export const products = [
       },
       {
         id: 1,
-        name: 'C75 HITAM (0.45)',
-        weight: '+-2.7kg - 2.55kg',
+        name: 'C75 PINK (0.50)',
+        weight: '+-3.0kg - 2.8kg',
       },
       {
         id: 2,
-        name: 'C75 HITAM (0.45)',
-        weight: '+-2.7kg - 2.55kg',
+        name: 'C75 MERAH (0.55)',
+        weight: '+-3.4kg - 3.2kg',
+      },
+      {
+        id: 3,
+        name: 'C75 ORANGE (0.60)',
+        weight: '+-3.7kg - 3.55kg',
+      },
+      {
+        id: 4,
+        name: 'C75 KUNING (0.65)',
+        weight: '+-4.1kg - 3.9kg',
       },
     ],
   },
