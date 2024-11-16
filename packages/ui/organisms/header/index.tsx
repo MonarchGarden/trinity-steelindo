c065560b83feabedf915b4e6e846be9b2656949f
TheChefBigDog <bobbyryan692@gmail.com>
2024-11-16 11:35:11 +0700

    Checkpoint

--- packages/ui/organisms/header/index.tsx
+++ packages/ui/organisms/header/index.tsx
@@ -19,7 +19,11 @@ export const Header = ({logoWhite, logoBlack, headerOpacity}: Props) => {
 
   const toggleMenu = useCallback(() => {
     setIsMenuOpen((prevState) => !prevState);
-    document.body.style.overflow = !isMenuOpen ? 'hidden' : 'auto';
+    if (!isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
   }, [isMenuOpen]);
 
   useEffect(() => {
@@ -29,65 +33,80 @@ export const Header = ({logoWhite, logoBlack, headerOpacity}: Props) => {
   }, []);
 
   return (
-    <nav className="border-gray-200 bg-gray-50 dark:border-gray-700 dark:bg-gray-800">
-      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-        <div className="flex items-center space-x-3 rtl:space-x-reverse">
+    <header
+      className={`${headerOpacity < 0.7 ? 'opacity-100' : 'opacity-0'} ${headerOpacity > 0.9 ? 'bg-white text-black opacity-100' : 'opacity-0'} 
+      group fixed top-0 w-full overflow-hidden p-3 transition-all hover:bg-white group-hover:text-black md:flex ${isMenuOpen ? 'h-screen' : ''}`}
+      onMouseEnter={() => setHeaderHovered(true)}
+      onMouseLeave={() => setHeaderHovered(false)}>
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="flex items-center">
           <img
-            src={isHeaderHovered || headerOpacity > 0.4 ? logoWhite : logoBlack}
-            className="h-8"
-            alt="Logo"
+            src={isHeaderHovered || headerOpacity > 0.9 ? logoWhite : logoBlack}
+            className="h-20 w-20"
           />
           <span
-            className={`self-center whitespace-nowrap text-2xl font-semibold ${isHeaderHovered || headerOpacity > 0.4 ? 'text-black' : 'text-white'} dark:text-white`}>
+            className={`self-center whitespace-nowrap text-2xl font-bold sm:text-lg md:whitespace-nowrap md:text-xl ${
+              isHeaderHovered || headerOpacity > 0.9
+                ? 'text-colorPrimary'
+                : 'text-white'
+            } trinity-title-text whitespace-pre-line font-helios-condensed`}>
             TRINITY{'\n'}STEELINDO
           </span>
         </div>
-        <button
-          data-collapse-toggle="navbar-solid-bg"
-          type="button"
-          className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-          aria-controls="navbar-solid-bg"
-          aria-expanded={isMenuOpen ? 'true' : 'false'}
-          onClick={toggleMenu}>
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="h-5 w-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14">
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
+        <div className="ml-auto flex items-center">
+          <nav className="desktop-views-nav-menu space-x-4 font-helios-condensed">
+            <NavButtonLink
+              navBarTitle="Beranda"
+              isHeaderHovered={isHeaderHovered}
+              headerOpacity={headerOpacity}
             />
-          </svg>
-        </button>
-        <div
-          className={`${
-            isMenuOpen ? 'block' : 'hidden'
-          } w-full md:block md:w-auto`}
-          id="navbar-solid-bg">
-          <ul className="mt-4 flex flex-col rounded-lg bg-gray-50 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-transparent md:dark:bg-transparent rtl:space-x-reverse">
-            <li>
-              <NavButtonLink
-                navBarTitle="Beranda"
-                isHeaderHovered={isHeaderHovered}
-                headerOpacity={headerOpacity}
-              />
-            </li>
-            <li>
-              <NavButtonLink
-                navBarTitle="Katalog Produk"
-                isHeaderHovered={isHeaderHovered}
-                headerOpacity={headerOpacity}
-              />
-            </li>
-          </ul>
+            <NavButtonLink
+              navBarTitle="Katalog Produk"
+              isHeaderHovered={isHeaderHovered}
+              headerOpacity={headerOpacity}
+            />
+          </nav>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="mobile-views-nav-menu h-10 w-10 items-center justify-center rounded-lg p-2 align-middle text-gray-500 hover:bg-colorSecondary focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded={isMenuOpen ? 'true' : 'false'}
+            onClick={toggleMenu}>
+            {isMenuOpen ? (
+              // X icon
+              <svg
+                className="h-7 w-7"
+                aria-hidden="true"
+                fill="none"
+                viewBox="0 0 24 24">
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              // Elypsis button
+              <svg
+                className="h-5 w-5"
+                aria-hidden="true"
+                fill="none"
+                viewBox="0 0 17 14">
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            )}
+          </button>
         </div>
       </div>
-    </nav>
+    </header>
   );
 };
--- src/AppPresenter.tsx
+++ src/AppPresenter.tsx
@@ -100,106 +100,114 @@ export default function App() {
       {loading ? (
         <SplashScreen />
       ) : (
-        // <div className="no-scrollbar relative flex h-full w-full flex-col overflow-auto bg-colorPrimary">
-        //   {/* Mobile Version */}
-        //   <div className="display-background-mobile relative h-screen w-full overflow-hidden bg-IconTrinityTruckSecondFull bg-cover bg-center bg-no-repeat">
-        //     {/* Gradient Top */}
-        //     <div className="absolute inset-0 bg-gradient-to-t from-transparent to-colorPrimary" />
-        //     {/* Curtain Shadows */}
-        //     <div className="absolute inset-0 bg-colorPrimary bg-opacity-40" />
-        //     <div className="group-textbox absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center pb-5">
-        //       {/* Responsive font size */}
-        //       <h1 className="trinity-title-text whitespace-pre-line text-center font-helios-condensed font-bold text-white">
-        //         We Are Ready{'\n'}To Satisfy Your Needs
-        //       </h1>
-        //       {/* Responsive text */}
-        //     </div>
-        //   </div>
-
-        //   {/* Desktop and Tablet Version bg-IconTrinityTruckSecond*/}
-        //   <div className="bg-IconTrinityTruckSecond display-background-desktop relative hidden h-screen w-full overflow-auto overflow-y-auto bg-cover bg-center bg-no-repeat sm:block">
-        //     {/* Gradient Top */}
-        //     <div className="absolute inset-0 bg-gradient-to-t from-transparent to-colorPrimary" />
-        //     {/* Curtain Shadows */}
-        //     <div className="absolute inset-0 bg-colorPrimary bg-opacity-30" />
-        //     <div className="group-textbox bottom-0 left-0 flex flex-col items-center px-10 sm:hidden md:absolute md:bottom-0 md:right-0 md:flex md:items-start md:px-10">
-        //       {/* Responsive font size */}
-        //       <h1 className="trinity-title-text whitespace-pre-line pb-5 font-helios-condensed font-bold text-white">
-        //         We Are Ready{'\n'}To Satisfy Your Needs
-        //       </h1>
-        //       {/* Responsive text */}
-        //       <h3 className="mb-10 hidden whitespace-pre-line text-justify font-helios-condensed text-xl text-white md:mb-5 md:flex md:text-xl lg:pb-10 lg:text-xl xl:pb-10 xl:text-2xl">
-        //         Trinity Steelindo is a trusted destination for high-quality
-        //         steel and UPVC roofing products.
-        //         {'\n'}We are committed to excellence and offer a wide range of
-        //         steel products {'\n'}to meet the diverse needs of the industrial
-        //         and construction sectors.
-        //       </h3>
-        //     </div>
-        //   </div>
-        //   <Body>
-        //     <div
-        //       className={`flex flex-col items-center justify-center text-xl sm:block`}>
-        //       <div className="w-full overflow-hidden whitespace-nowrap  text-center">
-        //         <h1
-        //           className={`${
-        //             showsOnce ? 'fill-text-title' : 'hidden'
-        //           } overflow-hidden pb-5 font-helios-condensed text-4xl text-colorTitle`}>
-        //           Siapa Kami?
-        //         </h1>
-        //       </div>
-        //       <div
-        //         className={`${
-        //           showsOnce ? 'description-animation' : 'hidden'
-        //         } w-full items-center justify-center  pb-5`}>
-        //         <h6 className="mb-8 w-full scale-100 transform text-center font-helios-condensed text-white transition-transform sm:mb-0">
-        //           Upgrade your roofing needs with UPVC roofing products from us.
-        //           These products provide durability and aesthetic appeal,
-        //           ensuring that your projects are built to last. Join us as we
-        //           offer innovation, reliability, and top-notch customer
-        //           satisfaction in every product we provide.
-        //         </h6>
-        //       </div>
-        //       <div
-        //         className={`flex flex-col items-center justify-center  text-xl text-colorDescription`}>
-        //         <div
-        //           className={`flex flex-col ${
-        //             showsOnce ? 'daily-activity-animation' : 'hidden'
-        //           } items-center rounded pb-5 text-center`}>
-        //           <h2 className="relative pb-5 font-helios-condensed text-2xl font-semibold">
-        //             Our Activities
-        //             <div className="-bottom-px h-1 bg-gradient-to-r from-colorDescription via-transparent to-transparent"></div>
-        //           </h2>
-        //           <Carousel
-        //             transition={{duration: 2}}
-        //             className="h-48 w-full items-center rounded-md sm:w-1/2 xl:h-96"
-        //             placeholder={undefined}
-        //             onPointerEnterCapture={undefined}
-        //             onPointerLeaveCapture={undefined}>
-        //             {listOfImageCarousel}
-        //           </Carousel>
-        //         </div>
-        //         <div
-        //           className={`${
-        //             showsOnce ? 'founder-text-title' : 'hidden'
-        //           } flex items-center justify-center pb-5 text-center text-sm font-extralight`}>
-        //           Trinity SteelIndo
-        //         </div>
-        //       </div>
-        //     </div>
-        //   </Body>
-        //   <div className="hidden h-full w-full flex-col overflow-y-auto bg-colorPrimary px-5 py-5 lg:flex xl:flex">
-        //     {listOfMainProductsDesktopViews}
-        //   </div>
-        //   <div className="mobile-tablet-views w-full gap-5 bg-colorPrimary p-8 lg:hidden xl:hidden">
-        //     {listOfMainProductsMobileViews}
-        //   </div>
-        <Header
+        <div className="no-scrollbar relative flex h-full w-full flex-col overflow-auto bg-colorPrimary">
+          {/* <Header
           headerOpacity={innerHeightScrollPosition}
           logoBlack={IconLogoTrinityTrans}
           logoWhite={IconLogoTrinityWhiteTrans}
-        />
-        // </div>
+        /> */}
+
+          {/* Mobile Version */}
+          <div className="display-background-mobile relative h-screen w-full overflow-hidden bg-IconTrinityTruckSecondFull bg-cover bg-center bg-no-repeat">
+            {/* Gradient Top */}
+            <div className="absolute inset-0 bg-gradient-to-t from-transparent to-colorPrimary" />
+            {/* Curtain Shadows */}
+            <div className="absolute inset-0 bg-colorPrimary bg-opacity-40" />
+            <div className="group-textbox absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center pb-5">
+              {/* Responsive font size */}
+              <h1 className="trinity-title-text whitespace-pre-line text-center font-helios-condensed font-bold text-white">
+                We Are Ready{'\n'}To Satisfy Your Needs
+              </h1>
+              {/* Responsive text */}
+            </div>
+          </div>
+
+          {/* Desktop and Tablet Version bg-IconTrinityTruckSecond*/}
+          <div className="bg-IconTrinityTruckSecond display-background-desktop relative hidden h-screen w-full overflow-auto overflow-y-auto bg-cover bg-center bg-no-repeat sm:block">
+            {/* Gradient Top */}
+            <div className="absolute inset-0 bg-gradient-to-t from-transparent to-colorPrimary" />
+            {/* Curtain Shadows */}
+            <div className="absolute inset-0 bg-colorPrimary bg-opacity-30" />
+            <div className="group-textbox bottom-0 left-0 flex flex-col items-center px-10 sm:hidden md:absolute md:bottom-0 md:right-0 md:flex md:items-start md:px-10">
+              {/* Responsive font size */}
+              <h1 className="trinity-title-text whitespace-pre-line pb-5 font-helios-condensed font-bold text-white">
+                We Are Ready{'\n'}To Satisfy Your Needs
+              </h1>
+              {/* Responsive text */}
+              <h3 className="mb-10 hidden whitespace-pre-line text-justify font-helios-condensed text-xl text-white md:mb-5 md:flex md:text-xl lg:pb-10 lg:text-xl xl:pb-10 xl:text-2xl">
+                Trinity Steelindo is a trusted destination for high-quality
+                steel and UPVC roofing products.
+                {'\n'}We are committed to excellence and offer a wide range of
+                steel products {'\n'}to meet the diverse needs of the industrial
+                and construction sectors.
+              </h3>
+            </div>
+          </div>
+
+          <Body>
+            <div
+              className={`flex flex-col items-center justify-center text-xl sm:block`}>
+              <div className="w-full overflow-hidden whitespace-nowrap  text-center">
+                <h1
+                  className={`${
+                    showsOnce ? 'fill-text-title' : 'hidden'
+                  } overflow-hidden pb-5 font-helios-condensed text-4xl text-colorTitle`}>
+                  Siapa Kami?
+                </h1>
+              </div>
+              <div
+                className={`${
+                  showsOnce ? 'description-animation' : 'hidden'
+                } w-full items-center justify-center  pb-5`}>
+                <h6 className="mb-8 w-full scale-100 transform text-center font-helios-condensed text-white transition-transform sm:mb-0">
+                  Upgrade your roofing needs with UPVC roofing products from us.
+                  These products provide durability and aesthetic appeal,
+                  ensuring that your projects are built to last. Join us as we
+                  offer innovation, reliability, and top-notch customer
+                  satisfaction in every product we provide.
+                </h6>
+              </div>
+              <div
+                className={`flex flex-col items-center justify-center  text-xl text-colorDescription`}>
+                <div
+                  className={`flex flex-col ${
+                    showsOnce ? 'daily-activity-animation' : 'hidden'
+                  } items-center rounded pb-5 text-center`}>
+                  <h2 className="relative pb-5 font-helios-condensed text-2xl font-semibold">
+                    Our Activities
+                    <div className="-bottom-px h-1 bg-gradient-to-r from-colorDescription via-transparent to-transparent"></div>
+                  </h2>
+                  <Carousel
+                    transition={{duration: 2}}
+                    className="h-48 w-full items-center rounded-md sm:w-1/2 xl:h-96"
+                    placeholder={undefined}
+                    onPointerEnterCapture={undefined}
+                    onPointerLeaveCapture={undefined}>
+                    {listOfImageCarousel}
+                  </Carousel>
+                </div>
+                <div
+                  className={`${
+                    showsOnce ? 'founder-text-title' : 'hidden'
+                  } flex items-center justify-center pb-5 text-center text-sm font-extralight`}>
+                  Trinity SteelIndo
+                </div>
+              </div>
+            </div>
+          </Body>
+
+          <div className="hidden h-full w-full flex-col overflow-y-auto bg-colorPrimary px-5 py-5 lg:flex xl:flex">
+            {listOfMainProductsDesktopViews}
+          </div>
+          <div className="mobile-tablet-views w-full gap-5 bg-colorPrimary p-8 lg:hidden xl:hidden">
+            {listOfMainProductsMobileViews}
+          </div>
+          <Header
+            headerOpacity={innerHeightScrollPosition}
+            logoBlack={IconLogoTrinityTrans}
+            logoWhite={IconLogoTrinityWhiteTrans}
+          />
+        </div>
       )}
     </>
   );
