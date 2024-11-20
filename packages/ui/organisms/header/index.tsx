089ad8afd9d65ebf0b9004ea4cace82a7c16efb4
TheChefBigDog <bobbyryan692@gmail.com>
2024-11-20 12:03:40 +0700

    Mobile Check

--- packages/ui/organisms/header/index.tsx
+++ packages/ui/organisms/header/index.tsx
@@ -17,11 +17,20 @@ export const Header = ({logoWhite, logoBlack}: Props) => {
       setScrollPosition(Math.min(window.scrollY, 200));
     };
 
+    const handleResize = () => {
+      // Close mobile menu if screen width exceeds 768px (md breakpoint)
+      if (window.innerWidth >= 768 && isMobileMenuOpen) {
+        setIsMobileMenuOpen(false);
+      }
+    };
+
     window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
     };
-  }, []);
+  }, [isMobileMenuOpen]);
 
   const headerOpacity = scrollPosition / 200;
   const isScrolled = headerOpacity > 0.2;
@@ -75,9 +84,40 @@ export const Header = ({logoWhite, logoBlack}: Props) => {
 
         {/* Mobile Menu Toggle */}
         <button
-          className="block p-2 text-black md:hidden"
-          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
-          <span className="sr-only">Toggle Menu</span>☰
+          className={`block p-2 ${isScrolled || isHovered || isMobileMenuOpen ? 'text-black' : 'text-white'}  md:hidden`}
+          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+          aria-label="Toggle Menu">
+          {isMobileMenuOpen ? (
+            // Close Icon SVG
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            // Hamburger Menu SVG
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
         </button>
       </div>
 
