7de0cf1279b3d50579de9b01a93bc589aaf0dbba
TheChefBigDog <bobbyryan692@gmail.com>
2026-01-11 18:11:54 +0700

    Commit of the day:
     + Beranda Navigation has been updated
    
     + FIrebase Image has been Added
    
     + Adjusted the product table section

--- src/services/catalog_service.ts
+++ src/services/catalog_service.ts
@@ -1,5 +1,6 @@
 import {fetchAndActivate, getValue} from 'firebase/remote-config';
 import {getRemoteConfigInstance} from '../firebase/initFirebase';
+import {catalogSchema} from '../schemas/catalog.schema';
 
 export async function fetchCatalogJson() {
   const rc = getRemoteConfigInstance();
@@ -10,5 +11,9 @@ export async function fetchCatalogJson() {
 
   const res = await fetch(url);
   if (!res.ok) throw new Error(`Fetch catalog failed: ${res.status}`);
-  return res.json();
+
+  const json = await res.json();
+  const parsed = catalogSchema.parse(json);
+
+  return parsed.tables;
 }
